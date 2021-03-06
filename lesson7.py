8f7171d1bce3e0691dcb76492a1d5a2ed7ddf44b
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:06:53 +0400

    yoterord

---
+++ lesson7.py
@@ -0,0 +1,66 @@
+# age = int(input('Age:  '))
+# rays_age = age > 18 and age <= 40
+
+# ser = input('Ser(f or m):  ').upper() == 'F'
+
+# krtutyuny = input('Krtutyuny(b- barcraguyn)').upper() == 'B'
+# verjnakany = rays_age and ser and krtutyuny
+# print(verjnakany)
+# x = 4	
+# Good = x < 5 or x > 8 and  x > 10
+# print(Good)
+# x = 4 
+# aa = is not(x < 5 or x < 10)
+# a = 'a'
+# b = 'a'
+# print(a is b) # nuynnen popoxakannery te che
+# print(id(a))
+# print(id(b)) # popoxakani hascen hishoxutyan mej
+# print(ord('a')) # a-i specefik tivy klavyaturai vra
+# print(ord("A")) # A-i specefik tivy klavyaturai vra
+# print(chr(97)) # nuynya tvi depqum cuca tali specefik tivy
+# pc = "Apple Asus Dell HP"
+# user = input('Pc?(unenq verevi toxum?')
+# res = user  in pc # ka et tiv/tary verevi popoxakaknum True
+# print(res)
+# pc = "Apple Asus Dell HP"
+# user = input('Pc?(unenq verevi toxum?')
+# res = user not in pc # ete chka et popoxakanum True
+# print(res)
+
+# b = input('Banan:  ').upper() == 'YES'
+# a = input('Apple: ').upper() == 'YES'
+# o = input('Orange: ').upper() == 'YES'
+# print('Banan-->', b, 'Orange-->', o , 'Apple-->', a)
+# light = input('Uneq luys?').upper() == 'YES'
+# res_b =  b and not light
+# res_o =  o and not light
+# res_a =  a and not light
+
+# print('Vekalenq xaladelnikic', res_b)
+# print('Vekalenq xaladelnikic', res_o)
+# print('Vekalenq xaladelnikic', res_a)
+# arnel = True
+# o_arnel = arnel and o  # karanq grenq not ov
+# a_arnel = arnel and a 
+# b_arnel = arnel and b 
+# print('Gnel orange-->',orange,'Gnel banan-->', banan,'Gnel xndzor-->',a\ )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
