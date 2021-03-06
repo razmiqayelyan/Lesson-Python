439c0d7c8812787f61a178a1885d52b69b97fa6f
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:10:46 +0400

    taserord

---
+++ lesson10.py
@@ -0,0 +1,36 @@
+# if year % 4 == 0:
+# 	if year % 400 == 0 or year % 100 != 0:
+# 		print('year')  # nahanj tari
+
+# x = int(input("Dzer tivy:  "))
+# first = num % 10
+# second = num // 10 % 10
+# third = num // 100 % 10  # frsnuma tivy
+# fourth = num // 1000
+
+# print(str(first) + str(second) + str(third) + str(fourth))
+# print(str(num)[::-1])
+
+# jampa = 12 
+# pc = 3 
+# pc += pc * 0.1
+# es = 12 / 3.3
+# print(es)
+
+import random
+
+pc_folow = random.randint(100,1000)
+im_folow = int(input('Qo wollowrnery:  '))
+if pc_folow * 0.2 + pc_folow >= im_folow:
+	print('Du bloger es')
+else:
+	print('You arent bloger')
+
+
+
+
+
+
+
+
+
