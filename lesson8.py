9d9fdb0cbc4a61e6f7577d1a054bf56869acf034
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:08:21 +0400

    uterord

---
+++ lesson8.py
@@ -0,0 +1,68 @@
+# model = input('Do you have Iphone 12:yes/no  ').lower() == 'yes'
+# color = input('I want color Gold,you have?:  ').lower() == 'yes'
+# ete = color and model 
+# print('You can buy this item from our store-->', ete)
+
+# question1 = input('Qani nahang uni AMN-n \na)10, b)40, c)90, d)50  ').lower() == 'd'
+# print('Harcin patasxanel eq-->', question1)
+# question2 = input('Qani bjjayin operator ka Hayastanum \na)1, b)2 c)3, d)4  ').lower() == 'c'
+# print('Harcin patasxanel eq-->', question2)
+# question3 = input(" '1' + '1'= \na)11, b)1,  c)2,  d)5    ").lower() == 'a'
+# print('Harcin patasxanel eq-->', question3)
+# question4 = input('121-i armaty \na)11, b)1,  c)2,  d)5  ').lower() == 'a'
+# print('Harcin patasxanel eq-->', question4)
+# question5 = input('Yntreq amenalav rap-ernerin \na)Misho, b)Miyagi & Vnasakar c)Guf d)Basta  ').lower() == 'b'
+# print('Harcin patasxanel eq-->', question5)
+
+# age = int(input("Your age:  "))
+# tariq = age >= 17 
+# tariq2 = age <= 25 
+# ete = tariq == tariq2
+# print('Dzer tariqy hamapatasxanum e -->', ete)
+
+# auto_salon = 'BMW , AUDI , Mercedes'
+# car_date = '2020 , 2021 , 1999'
+
+# car = input('What car you want?  ').upper()
+# date = input('Inch tvi?  ')
+# res1 = date in car_date
+# res2 = car in auto_salon
+# ete = res1 and res2
+# print('we have this car-->', ete) 
+# import random 
+
+# mer_tivy = '1, 2, 5, 9, 8'
+# komp = random.randint(0,10)
+# print('Hamakarchi tivy-->', komp)
+# ete = str(komp) in mer_tivy
+# print('Tivy gtnvum e cucakum-->', ete)
+
+# import math 
+
+# tiv = int(input('Number:  '))
+# fak = math.factorial(tiv)
+# print(fak)
+gram = float(input('Qani gram?  '))
+sigaret = gram // 20
+print('Duq karox eq artadrel aysqan sigaret-->', sigaret)
+mnacordy = gram %  20
+print('Mnacordy klini --> ', mnacordy)
+tup = sigaret // 20
+print('aysqan tup--:>', tup)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
