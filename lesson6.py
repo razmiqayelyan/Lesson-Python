aca6fb32c02608e8a5da2d14509ebe36dfa7f395
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:01:29 +0400

    vecerord

---
+++ lesson6.py
@@ -0,0 +1,137 @@
+# import datetime
+
+# x = datetime.datetime.now()
+
+
+
+
+
+# import math
+# from math import * #sax grum enq aranc math.
+# from math import pi # el pi-i demy math. chenq grum
+
+
+# print(math.pi)
+# print(math.sqrt(16)) # asuma 16-i armaty
+# # c = float(input())
+# # sharavixy = 2 * c * math.pi #sharavixy = 2pr
+# # print(sharavixy)
+# import random 
+# print(random.random()) # 0-1 patahakan tiv
+# print(random.randint(0, 10))
+# komp_1 = random.randint(0, 10)
+# print('Dzer tivy', komp_1)
+# komp_2 = random.randint(0, 10)
+# print('Dzer tivy', komp_2)
+# winner = komp_1 >= komp_2
+# print(winner,'False--> haxtec arajiny', 'True--> haxtec erkrordy')
+# komp_1 = random.randint(2,12)
+# komp_4 =  random.randint(2,12)
+# a = komp_1 
+# b = komp_4 
+# c = a >= b
+# print(c)
+# import random 
+# x = 'abcdefgh'
+# print(random.choice(x))
+
+# abc = 'hsadjfajhsgdfhjs'
+# number = '12316545641'
+# password = ''
+# x = random.choice(abc)
+# password  += x
+# x = random.choice(abc)
+# password  += x
+# x = random.choice(abc)
+# password  += x
+# x = random.choice(abc)
+# password  += x
+# x = random.choice(number)
+# password  += x
+# x = random.choice(number)
+# password  += x
+# print(password)
+
+# import random as r #vor el chgrenq random
+
+# print(r.randrange(0,100,5)) # 0,5,10,15,20
+# print(r.randrange(0,100,2)) # zuyqeric inchvormeky tpuma
+# import time
+# print('Hi Razo')
+# time.sleep(2)
+# print('Hi Arno whatsapp')
+# time.sleep(0.9)
+# print('Good')
+# import os
+# print('Path-->', os.getcwd()) #urdea qo file y gtnvum
+#                                # vory hmi baca(lesson6.py)
+# # import calendar
+# # y = int(input('Input the year:  '))
+# # m = int(input('Input the month:  '))
+# # print(calendar.month(y,m))
+
+# import datetime
+
+# x = datetime.datetime.now()
+# print(x)
+# print(datetime.date.today())
+# endtime = start = datetime.datetime.now()
+# verjnakan = endtime - start
+# print(verjnakan)
+# age = int(input("Erb eq cnvel?  "))
+# x = datetime.datetime.now()
+# tariq = x.year - age
+# print(tariq)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
