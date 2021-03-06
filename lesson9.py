06ae7dbff59b30cdd2e35fb1b0d001f38c1e42e2
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:09:49 +0400

    innerord

---
+++ lesson9.py
@@ -0,0 +1,104 @@
+# a = 27 
+# b = 14
+# if a > b:
+# 	print('a is big')
+# Jack = int(input('Your age:  '))
+# Razo = int(input('Your age:  '))
+# if Razo > Jack:
+# 	print('Urish tarberak chere kara liner')
+# if Razo < Jack:
+# 	print('False')
+
+# Ani = int(input('age: Ani:  '))
+# Razo = int(input('age: Razo:  '))
+# if Ani > Razo:
+# 	print('Ani is older')
+# elif Razo > Ani:
+# 	print('Razo is older')
+# else:
+# 	print('Ani == Razo')
+
+# question = input('Dzer harcy?  ')
+# if question == 'inche im anuny':
+# 	print('Arno')
+# elif 'tarekan' in question:
+# 	print('26')
+# else:
+#     print('chunem patasxan')
+
+# Jack = int(input("Jack's age:  "))
+# Jnjxuk = int(input("Jnjxuk's age:  "))
+# Razo = int(input("Razo's age:  "))
+
+# if Jack > Razo and Jack > Jnjxuk:
+# 	print('old is Jack')
+# if Razo > Jack and Razo > Jnjxuk:
+# 	print('old is Razo')
+# if Jnjxuk > Jack and Jnjxuk > Razo:
+# 	print('old is Jnjxuk')	
+# if Jack == Jnjxuk == Razo:
+# 	print('Sax havasar en')
+# if Jack == Jnjxuk and Jack < Razo :
+# 	print('Razo is older')
+# if Jack == Jnjxuk and Jack > Razo:
+# 	print('Jack is older')
+
+# isalpha - cuca tali tara te che
+# isdigit - cuca tali tiva te che
+# tar = input('Name:  ')
+# if char.isalpha():
+# 	print('Good')
+# else:
+# 	print('Please input letters')
+
+# age = input('Age:  ')
+# if age.isdigit():
+# 	age = int(age)
+# 	print(2021 - age)
+# else:
+# 	print('Please input number')
+
+# x = input('char:  a')
+# if x.isupper():        #mecatar
+# 	print('Good')
+
+# x = input('char:  ')
+# if x.islower():        #poqratar
+# 	print('good')
+
+dog = int(input('Dogs year:  '))
+if dog < 0:
+	print('Error')
+elif dog >= 0 and dog <= 2:
+	mard = dog * 5.25
+	print('Mardu tariqy-->', mard)
+else: mard = 10.5 + 4 * (dog - 2):
+	print('Mardu tariqy', mard)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
