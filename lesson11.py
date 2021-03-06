a52a01ccc9a9e951bb2c7496c0b5f159a64a0b44
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:11:39 +0400

    tasnmekerord

---
+++ lesson11.py
@@ -0,0 +1,47 @@
+# names = 'Artur'
+# for name in names:
+# 	print(name)
+
+# name = "Albert"
+
+# for i in name:
+# 	print(i)
+# 	if i == 'l':
+# 		break
+
+import time
+ 
+n = 1
+while True:
+	n *= 2
+	if n >= 3:
+		print(n)
+		break
+	print(n)
+	n -=1
+	continue
+
+	time.sleep(0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
