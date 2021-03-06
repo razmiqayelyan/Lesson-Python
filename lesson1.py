9b109ac4cc60604037e8acb20ec80fb76813d053
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 16:46:03 +0400

    arajin das

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+#print(" Hello World ")
+#name = 'Ani'
+#print(name)
+#name = input("What's your name")
+#age = input('How old are you?')
+#phone = input('Phone: ')
+#print('Hello',name, 'age', age , 'phone',phone,'email',email)
+
+
+#born = int(input('Ebes cnve?'))
+#print(2021 - born)
+# a = int(input('erk'))
+# b = int(input('layn'))
+# s = a * b
+# print(s)
+# \n - taza toxic
+# \t - mi mat xorqic
+
+# a = int(input('erk'))
+# h = int(input('barcr'))
+# b = 0.5
+# s = b * a * h
+# print(s)
+print(0.1 + 0.2)
+
+
