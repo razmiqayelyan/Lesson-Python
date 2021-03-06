ea6de106286f4313fa0f9ab41c9495130c4a38bd
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 16:59:26 +0400

    chorord

---
+++ lesson4.py
@@ -0,0 +1,88 @@
+# % - mnacordy
+#  // - amboxjy
+# ** - barcracnel astijan
+# x = 7 
+# y = 5 
+# result = x % y 
+# print(result)
+# x = 10 
+# y = 3 
+# result = x // y
+# print(result)
+# register = int(input('Qani andz e grancvel:  '))
+# xumb = int(input('Qani xumb eq cankanum stexcel:  '))
+# tivy = register % xumb
+# print('kmna xmbic durs', tivy) 
+# mnacordy = register // xumb 
+# print( mnacordy,  'xumb')
+# x = 4 
+# y = 3 
+# result = x ** y
+# y = 0.5
+# result = x ** 0.5
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+# ab_qarakusin = (a + b) **2
+# print(ab_qarakusin)
+# balance = 100
+# print(balance)
+# paycheck = 150
+# balance = balance + paycheck
+# print(balance)
+# balance = 100
+# print(balance)
+# balance += 150
+# print(balance)
+# balance = 0
+# telcell = int(input('Inchqan eq cankanum avelacnel: '))
+# balance += telcell
+# print(balance)
+# shoppingy = int(input('Inchqan caxseciq aysor?: '))
+# res = balance >= shoppingy
+# print(res)
+# x = 5
+# y = 7 
+# x == y
+# x != y
+
+# tar = input()
+# res = tar == tar.upper()  # ete uzum enq poqratar lower()
+# print(res)   # asum enq mecatar ylni te che
+
+
+# balance = int(input('How money you have: '))
+# apranqi_arjeqy = int(input('Price:  ' ))
+# sale = int(input('Your sale in procents: '))
+# over_price = apranqi_arjeqy - apranqi_arjeqy*sale/100
+# klni = over_price <= balance
+# print(balance - over_price)
+# print(klni)
+print('   *\n  * *\n * * *\n* * * *\n * * *\n  * *\n   *\n     ')
+
+a = '0'
+b = '0'
+c = '0'
+3 = int(a + b +c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
