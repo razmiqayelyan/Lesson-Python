5384c9a1351c436b3402694083d0b122d840b6bd
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 16:58:38 +0400

    erord

---
+++ lesson3.py
@@ -0,0 +1,94 @@
+# myvar = "Hello"
+# print(myvar)
+# my_var = 'world'
+# print(my_var)
+# _my_var = "John"
+# print(_my_var)
+# myVar = 55
+# print(myVar)
+# MYVAR = -55
+# print(MYVAR)
+# myvar2 = 123
+# print(myvar2)
+# x = 5
+# y = 2.3
+# print(type(x))
+# print(type(y))
+# name = 'Ben'
+# print(type('Ben'))
+# res = True
+# res2 = False
+# print(type(res2),type(res))
+
+""" Problem 1 """
+# x = 5
+# print(type(x))
+
+"""" Problem 2 """
+
+# name = 'Ani'
+# print(name.upper())
+# print(name.lower())
+# question = input('\n When was a battle Avarayr \na)450\nb)500\nc)700\nd)451')
+# print('Dzer patasxany', question.upper()=="D")
+# result1 = question1.upper()=='D'
+# print('Dzer patasxany', result1)
+# Razosharc = input('\nQani tesaxcik kunena iphone 13y \
+# 	\na)4 \nb)10 \nc)50 \nd)kmna anpopox').upper() == 'A'
+# patasxany = (type(Razosharc))
+# print('Dzer patasxany', patasxany)
+# name = input('name: ')
+# age = int(input('age: '))
+
+# print('My name is' + name,'I am' + str(age), 'years old')
+
+# x = '5'
+# y = '5'
+# print(type(x))
+# print(type(y))
+# print(int(x) + int(y))
+
+# x = input('X: ')
+# print(x, type(x))
+# y = float(input('number: '))
+# print(y, type(y))
+# a = float(input('barcrutyuny?'))
+# b = float(input('himqy?'))
+# makeresy = a * b /2
+# print('Erankyan S-y havasara', makeresy)
+
+# naxnakan = float(input('Price'))
+# tokosy = float(input('Procent'))
+# b = 100
+# overprice = naxnakan - naxnakan * tokosy /b
+# print(overprice)
+
+# stacelem = float(input('Naxnakan'))
+# caxelem = float(input('Orva_Cashy'))
+# ekamuts = caxelem - stacelem
+
+Ani = float(input('Ani'))
+Anna = float(input('Anna'))
+Anjela = float(input('Anjela'))
+
+orva_arevtur = Ani + Anna + Anjela
+print(orva_arevtur)
+
+mutq = orva_arevtur - orva_arevtur * 15 / 100
+print(mutq)
+
+sahuyt = orva_arevtur - mutq
+print(sahuyt)
+
+
+
+
+
+
+
+
+
+
+
+
+
