2267e6931e357add1a02b8e5aa5029794043af8e
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-06 17:00:51 +0400

    hingerord

---
+++ lesson5.py
@@ -0,0 +1,61 @@
+# celsius = float(input('Celsius:  '))
+# farenheyt = 32 + celsius * 1.8
+# print(farenheyt) 
+# farenheyt = float(input('farenheyt'))
+# celsius = (farenheyt - 32) /1.8
+# print(celsius)
+# x = int(input())
+# razo = x % 400 == 0
+# print(razo)
+# rope = int(input('Ropen asa:  '))
+# varkyany = rope * 60
+# print(varkyany)
+# day = int(input('Qani or: '))
+# rope = day * 60 * 24 
+# varkyany = rope * 60
+# print(rope)
+# print(varkyany)
+# print(type(rope), type(varkyany))
+# tivy = int(input())
+# bajanarary = tivy % 4 == 0
+# baj_2 = tivy % 400 != 0
+# yndhanur_baj = bajanarary == baj_2
+# print(bajanarary)
+# uxankyun_1  = float(input("Ankyun 1:  "))
+# uxankyun_2 = float(input("Ankyun 2:  "))
+# uxankyun = uxankyun_1 + uxankyun_2==90
+# print(uxankyun)
+# nerqnaciq = int(input())
+# demi_ejy = nerqnaciq / 2
+# koxqi_ejy = (nerqnaciq**2 - demi_ejy**2) ** 0.5 #armat
+# print(koxqi_ejy) 
+# sharavixy = float(input('Nsheq sharavixy'))
+# pi = 3.14
+# tramagicy = sharavixy * pi * 2
+# verjnakany = tramagicy * 3
+# print(verjnakany)
+# kilometry = verjnakany / 100 / 1000
+# print(kilometry) 
+anuny = input()
+im_gushakacy = input()
+patasxany = anuny.upper() == im_gushakacy.upper()
+print(patasxany)
+
+
+# tivy = int(input('Nsheq tivy:  '))
+# verjnakany = tivy >= 100
+# print(verjnakany)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
