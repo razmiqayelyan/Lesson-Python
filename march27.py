bd96262358397c8005cf40c5cb79942cfdce2be4
razmiqayelyan <80157827+razmiqayelyan@users.noreply.github.com>
2021-03-27 22:13:30 +0400

    27.march.2021

---
+++ march27.py
@@ -0,0 +1,39 @@
+# def test(mylist, x = None):
+# 	try:
+# 		assert len(mylist) != 0, 'datark e' # error a barcrasnum
+# 		return sum(mylist)/len(mylist)
+# 	except AssertionError as e:
+# 		return e
+# mylist = []
+# print(test(mylist))
+# x = 3.564564
+# print(round(x,2)) #3.56
+# print(round(x)) #4 round()-klorasnuma
+# def test(x):
+# 	return x * x
+# res = [1,2,43,5]
+# res = list(map(test,res)) # for -i pes hertov fuma bazmapatkuma(qarakusina tali)
+# print(c)
+# print(eval('5 + 5 - 6')) # gumaruma tvery irar bayc ete mi stringi mej
+# from test import info
+
+# print(info['name']) #Miyagi
+
+# for i in info:	# Name:Miyagi.....
+# 	print(i,info[i])
+
+# from test import factorial as f
+# print(f(5))
+
+# from test import tpel
+
+# print(tpel('hi','barev'))
+# print(tpel('hi'))
+# print('barev', end='1234')
+
+# from test import fibonachi
+# fibonachi()
+
+# from test import fibon
+# print(fibon())
+		
